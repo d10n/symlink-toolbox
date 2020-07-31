4d0a0d75a1e9c06ec40207cf5b299585b4a12fbd
d10n <david@bitinvert.com>
2020-07-31 07:52:32 -0400

    Update tests

--- tests/mv-leave-trail-spec.sh
+++ tests/mv-leave-trail-spec.sh
@@ -33,7 +33,7 @@ test_mv_file_to_dir() { :
   touch a
   src='a'
   dst='test1/a'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # src is a link
   [[ ! -L "$dst" && -f "$dst" ]] || return 2 # dst is a file
   [[ "$src" -ef "$dst" ]] || return 3 # a links to dst
@@ -42,7 +42,7 @@ test_mv_file_to_dir() { :
 test_mv_dir_to_file() { :
   src='test1'
   dst='test3'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # src is a link
   [[ ! -L "$dst" && -d "$dst" ]] || return 2 # dst is a file
   [[ "$src" -ef "$dst" ]] || return 3 # a links to dst
@@ -53,12 +53,11 @@ test_mv_dir_to_dir() { :
   src='test1'
   dst='test3'
   dst_result='test3/test1'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # test1 is a link
   [[ ! -L "$dst_result" && -d "$dst_result" ]] || return 2 # test3 is a dir
   [[ "$src" -ef "$dst_result" ]] || return 3 # test1 links to test3
   [[ $(readlink "$src") != '/'* ]] || return 4 # test1 is not a link to an absolute path
-  true
 }
 test_mv_file_to_symdir() { :
   touch a
@@ -66,7 +65,7 @@ test_mv_file_to_symdir() { :
   src='a'
   dst='test3'
   dst_result='test3/a'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # a is a link
   [[ ! -L "$dst_result" && -f "$dst_result" ]] || return 2 # dst_result is a file
   [[ "$src" -ef "$dst_result" ]] || return 3 # src links to dst_result
@@ -75,7 +74,7 @@ test_mv_file_to_symdir() { :
 test_mv_file_to_file() { :
   src='test1/sub dir1/space file1'
   dst='test2/sub dir2/space file1a'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # src is a link
   [[ ! -L "$dst" && -f "$dst" ]] || return 2 # dst is a file
   [[ "$src" -ef "$dst" ]] || return 3 # a links to dst
@@ -103,7 +102,7 @@ test_mv_dir_to_symdir() { :
   src='test1/sub dir1'
   dst='test3/sub dir2'
   dst_result='test2/sub dir2/sub dir1'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # test1 is a link
   [[ ! -L "$dst_result" && -d "$dst_result" ]] || return 2 # test3 is a dir
   [[ "$src" -ef "$dst_result" ]] || return 3 # test1 links to test3
@@ -134,7 +133,7 @@ test_mv_relsymdir_to_symdir() { :
   src='test3/sub dir2'
   dst='test2/sub dir1a'
   dst_result='test1/sub dir1/sub dir2'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # src is a link
   [[ -L "$dst_result" && -d "$dst_result" ]] || return 2 # dst_result is a symlink to a dir
   [[ -L "$dst/sub dir2" && -d "$dst/sub dir2" ]] || return 3 # dst/sub dir2 is a dir
@@ -151,7 +150,7 @@ test_mv_abssymdir_to_symdir() { :
   src='test3/sub dir2'
   dst='test2/sub dir1a'
   dst_result='test1/sub dir1/sub dir2'
-  mv-leave-trail "$src" "$dst"
+  mv-leave-trail "$src" "$dst" || return
   [[ -L "$src" ]] || return 1 # src is a link
   [[ -L "$dst_result" && -d "$dst_result" ]] || return 2 # dst_result is a symlink to a dir
   [[ -L "$dst/sub dir2" && -d "$dst/sub dir2" ]] || return 3 # dst/sub dir2 is a dir
@@ -184,7 +183,7 @@ test_mv_file_to_dir__file_timestamps_preserved() { :;
   touch -h --date '@1500000002' src1/a
   [[ "$(stat -c %Y src1/a)" = 1500000002 ]] || return 5
 
-  mv-leave-trail src1/a dst1
+  mv-leave-trail src1/a dst1 || return
   ls -ld src1
   ls -ld src1/*
   ls -ld dst1
@@ -201,7 +200,7 @@ test_mv_file_to_file__file_timestamps_preserved() { :;
   touch -h --date '@1500000002' src1/a
   [[ "$(stat -c %Y src1/a)" = 1500000002 ]] || return 5
 
-  mv-leave-trail src1/a dst1/aa
+  mv-leave-trail src1/a dst1/aa || return
   ls -ld src1
   ls -ld src1/*
   ls -ld dst1
@@ -220,7 +219,7 @@ test_mv_file_to_symdir__file_timestamps_preserved() { :;
   touch -h --date '@1500000003' dst2
   [[ "$(stat -c %Y src1/a)" = 1500000002 ]] || return
 
-  mv-leave-trail src1/a dst2
+  mv-leave-trail src1/a dst2 || return
   ls -ld src1
   ls -ld src1/*
   ls -ld dst2
